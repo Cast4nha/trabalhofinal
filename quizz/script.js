f64ce4d5187dbcb8d95153e2d4084ef528b6da1e
Felipe <felipevcast@unifesspa.edu.br>
2022-11-07 19:00:09 -0300

    Adição do quiz

--- quizz/script.js
+++ quizz/script.js
@@ -1,140 +1,99 @@
-// Questões
-const Questions = [{
-    id: 0,
-    q: "What is capital of India?",
-    a: [{ text: "gandhinagar", isCorrect: false },
-        { text: "Surat", isCorrect: false },
-        { text: "Delhi", isCorrect: true },
-        { text: "mumbai", isCorrect: false }
-    ]
-
+let perguntas = [
+{
+	titulo: 'É possível votar em dois candidatos a presidência?',
+	alternativas: ['Sim','Não','Talvez','Somente no municipio'],
+	correta: 0
 },
 {
-    id: 1,
-    q: "What is the capital of Thailand?",
-    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
-        { text: "phuket", isCorrect: false },
-        { text: "Ayutthaya", isCorrect: false },
-        { text: "Bangkok", isCorrect: true }
-    ]
-
+	titulo: 'Fire',
+	alternativas: ['Fogo','Agua','Terra','Ar'],
+	correta: 0
 },
 {
-    id: 2,
-    q: "What is the capital of Gujarat",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
-    ]
-
+	titulo: 'Bird',
+	alternativas: ['Gato','Urubu','Pombo','Passaro'],
+	correta: 3
 }
-
 ]
 
-// Iniciar
-var start = true;
+let app = {
+start: function(){
 
+	this.Atualpos = 0;
+	this.Totalpontos = 0;
 
-function iterate(id) {
+	let alts = document.querySelectorAll('.alternativa');
+	alts.forEach((element,index)=>{
+		element.addEventListener('click', ()=>{
+			this.checaResposta(index);
+		})
+	})
+	this.atualizaPontos();
+	app.mostraquestao(perguntas[this.Atualpos]);
+},
 
-// Getting the result display section
-var result = document.getElementsByClassName("result");
-result[0].innerText = "";
+mostraquestao: function(q){
+	this.qatual = q;
+	// mostrando o titulo
+	let titleDiv = document.getElementById('titulo');
+	titleDiv.textContent = q.titulo;
+	// mostrando as alternativas
+	let alts = document.querySelectorAll('.alternativa');
+	alts.forEach(function(element,index){
+		element.textContent = q.alternativas[index];
+	})
 
-// Constante que puxa a questão
-const question = document.getElementById("question");
+},
 
+Proximaperg: function(){
+	this.Atualpos++;
+	if(this.Atualpos == perguntas.length){
+		this.Atualpos = 0;
+	}
+},
 
-// Setting the question text
-question.innerText = Questions[id].q;
+checaResposta: function(user){
+	if(this.qatual.correta == user){
+		console.log("Correta")
+		this.Totalpontos++;
+		this.mostraresposta(true);
+	}
+	else{
+		console.log("Errada")
+		this.mostraresposta(false);
+	}
+	this.atualizaPontos();
+	this.Proximaperg();
+	this.mostraquestao(perguntas[this.Atualpos]);
+},
 
-// Getting the options
-const op1 = document.getElementById('op1');
-const op2 = document.getElementById('op2');
-const op3 = document.getElementById('op3');
-const op4 = document.getElementById('op4');
+atualizaPontos: function(){
+    let scoreDiv = document.getElementById('pontos');
+    scoreDiv.textContent = `Sua pontuacao: ${this.Totalpontos}`;
+},
 
-
-// Providing option text
-op1.innerText = Questions[id].a[0].text;
-op2.innerText = Questions[id].a[1].text;
-op3.innerText = Questions[id].a[2].text;
-op4.innerText = Questions[id].a[3].text;
-
-// Providing the true or false value to the options
-op1.value = Questions[id].a[0].isCorrect;
-op2.value = Questions[id].a[1].isCorrect;
-op3.value = Questions[id].a[2].isCorrect;
-op4.value = Questions[id].a[3].isCorrect;
-
-var selected = "";
-
-// Show selection for op1
-op1.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightgoldenrodyellow";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op1.value;
-})
-
-// Show selection for op2
-op2.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightgoldenrodyellow";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op2.value;
-})
-
-// Show selection for op3
-op3.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightgoldenrodyellow";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op3.value;
-})
-
-// Show selection for op4
-op4.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightgoldenrodyellow";
-    selected = op4.value;
-})
-
-// Grabbing the evaluate button
-const evaluate = document.getElementsByClassName("evaluate");
-
-// Evaluate method
-evaluate[0].addEventListener("click", () => {
-    if (selected == "true") {
-        result[0].innerHTML = "True";
-        result[0].style.color = "green";
-    } else {
-        result[0].innerHTML = "False";
-        result[0].style.color = "red";
+mostraresposta: function (correto) {
+    let resultDiv = document.getElementById('result');
+    let result = '';
+    // formate a mensagem a ser exibida
+    if (correto) {
+      result = 'Resposta Correta!';
     }
-})
+    else {
+      // obtenha a questão atual
+      let pergunta = perguntas[this.Atualpos];
+      // obtenha o índice da resposta correta da questão atual
+      let cindice = pergunta.correta;
+      // obtenha o texto da resposta correta da questão atual
+      let ctexto = pergunta.alternativas[cindice];
+      result = `Incorreto! Resposta Correta: ${ctexto}`;
+    }
+    resultDiv.textContent = result;
+  }
+
+
 }
 
-if (start) {
-iterate("0");
-}
+app.start();
 
-// Next button and method
-const next = document.getElementsByClassName('next')[0];
-var id = 0;
 
-next.addEventListener("click", () => {
-start = false;
-if (id < 2) {
-    id++;
-    iterate(id);
-    console.log(id);
-}
-
-})
